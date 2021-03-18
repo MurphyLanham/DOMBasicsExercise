d83c5727e4d40ea69ef8e366cecd2258e5242439
Murphy Lanham <murphylanham@KCCCs-MacBook-Pro.local>
2021-03-18 10:07:35 -0400

    Commit w/o notes

---
+++ app.js
@@ -0,0 +1,11 @@
+//  1
+const portillos = document.getElementById(`portillos`)
+
+//  2
+const images = document.getElementsByTagName(`img`)
+
+// 3
+const centered = document.getElementsByClassName(`center`)
+
+// 4
+const ginosEast = document.querySelector
